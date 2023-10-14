e6518c03c0ec5d8ef1a586ba62d511d4b4b445f8
Eric Krukowski <erickruk@amazon.com>
2023-10-14 17:45:07 -0400

    lab complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,90 @@
 class Game:
     def __init__(self, title):
         self.title = title
+    
+    @property
+    def title(self):
+        return self._title
 
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0 and not hasattr(self, '_title'):
+            self._title = title
+        return self._title
+    
     def results(self):
-        pass
+        return [r for r in Result.all if r.game==self]
 
     def players(self):
-        pass
+        return list(set([r.player for r in Result.all if r.game==self]))
 
     def average_score(self, player):
-        pass
+        player_scores = [r.score for r in Result.all if r.player == player and r.game ==self]
+        return (sum(player_scores)/len(player_scores))
+
 
 class Player:
     def __init__(self, username):
         self.username = username
 
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+
     def results(self):
-        pass
+        return [r for r in Result.all if r.player == self]
 
     def games_played(self):
-        pass
+        return list(set([r.game for r in Result.all if r.player == self]))
 
     def played_game(self, game):
-        pass
+        return game in self.games_played()
 
     def num_times_played(self, game):
-        pass
+        return len([r for r in Result.all if r.game == game and r.player == self])
+    
+    @classmethod
+    def highest_scored(cls, game):
+        result_list = game.results()
+        result_list.sort(key=lambda r:r.score, reverse=True)
+        return result_list[0].player
 
 class Result:
+    all = []
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <=5000 and not hasattr(self, '_score'):
+            self._score = score
+
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, player):
+        if isinstance(player, Player):
+            self._player = player
+    
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self._game = game
